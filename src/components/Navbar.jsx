75ce178f284d9d5781f273ec471956514ffd6c72
“prakshh” <prakashdas18@gmail.com>
2022-01-12 12:11:02 +0530

    Navbar, Styled-components, MaterialUI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,65 @@
+import { Search } from '@material-ui/icons';
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: 60px;
+    /* background-color: pink; */
+`;
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+const Left = styled.div`
+    /* width: 33.3%; */
+    flex: 1;
+    display: flex;
+    align-items: center;
+`;
+const Language = styled.span`
+    font-size: 14;
+    cursor: pointer;
+`;
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+const Input = styled.input`
+    border: none;
+`;
+const Center = styled.div`
+    flex: 1;
+    text-align: center;
+`;
+const Logo = styled.h1`
+    font-weight: bold;
+`;
+const Right = styled.div`
+    flex: 1;
+`;
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <Container>
+            <Wrapper>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        <Input /> 
+                        <Search />
+                    </SearchContainer>
+                </Left>
+                <Center>
+                    <Logo>LAMA.</Logo>
+                </Center>
+                <Right>right</Right>
+            </Wrapper>
+        </Container>
     )
 }
 
