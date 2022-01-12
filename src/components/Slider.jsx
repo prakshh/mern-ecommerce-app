2092a160556f70f91978fa4a897494c116bb53dc
“prakshh” <prakashdas18@gmail.com>
2022-01-12 20:40:36 +0530

    props, for arrow directions

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -19,16 +19,18 @@ const Arrow = styled.div`
     position: absolute;
     top: 0;
     bottom: 0;
-    left: 10px;
+    /* left: 10px; */
+    left: ${props => props.direction === "left" && "10px"};
+    right: ${props => props.direction === "right" && "10px"};
     margin: auto;
 `
 const Slider = () => {
     return (
         <Container>
-            <Arrow>
+            <Arrow direction="left">
                 <ArrowLeftOutlined />
             </Arrow>
-            <Arrow>
+            <Arrow direction="right">
                 <ArrowRightOutlined />
             </Arrow>
         </Container>
