c1545f29b02714e6e184ba76901f17ab26cd9705
“prakshh” <prakashdas18@gmail.com>
2022-01-14 11:25:55 +0530

    first slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,7 +5,7 @@ const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
-    background-color: coral;
+    /* background-color: coral; */
     position: relative;
 `
 const Arrow = styled.div`
@@ -23,13 +23,64 @@ const Arrow = styled.div`
     left: ${props => props.direction === "left" && "10px"};
     right: ${props => props.direction === "right" && "10px"};
     margin: auto;
+    cursor: pointer;
+    opacity: 0.5;
+`;
+
+const Wrapper = styled.div`
+    height: 100%;
+`;
+const Slide = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
 `
+const ImgContainer = styled.div`
+    height: 100%;
+    flex: 1;
+`
+const Image = styled.img`
+    height: 80%;
+`
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`
+const Title = styled.h1`
+    font-size: 70px;
+`
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+`
+
 const Slider = () => {
     return (
         <Container>
             <Arrow direction="left">
                 <ArrowLeftOutlined />
             </Arrow>
+            <Wrapper>
+                <Slide>
+                    <ImgContainer>
+                        <Image src="slider1.png"/>
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>SUMMER SALE</Title>
+                        <Desc>DON'T COMPROMISE ON STYLE! GET 30% OFF FOR NEW ARRIVALS</Desc>
+                        <Button>SHOW NOW</Button>
+                    </InfoContainer>
+                </Slide>
+            </Wrapper>
             <Arrow direction="right">
                 <ArrowRightOutlined />
             </Arrow>
