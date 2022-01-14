d40e4853ca44b7721beb0f889ffc0fff6cf699da
“prakshh” <prakashdas18@gmail.com>
2022-01-14 21:25:49 +0530

    categories building started

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div``
+
+const CategoryItem = ({item}) => {
+    return (
+        <Container>
+            oh yeah baby
+        </Container>
+    )
+}
+
+export default CategoryItem
