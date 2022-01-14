5a6b323876d0d5029074d21233d2336903a107e7
“prakshh” <prakashdas18@gmail.com>
2022-01-14 22:49:01 +0530

    category containers of images

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,11 +1,24 @@
 import styled from "styled-components";
 
-const Container = styled.div``
+const Container = styled.div`
+`
+const Image = styled.img`
+`
+const Info = styled.div`
+`
+const Title = styled.h1`
+`
+const Button = styled.button`
+`
 
 const CategoryItem = ({item}) => {
     return (
         <Container>
-            oh yeah baby
+            <Image src={item.img} />
+            <Info>
+                <Title>{item.title}</Title>
+                <Button>SHOP NOW</Button>
+            </Info>
         </Container>
     )
 }
