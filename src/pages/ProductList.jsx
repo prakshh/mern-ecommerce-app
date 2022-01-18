4df3f7e908286f3742614b035534d2f94360dfeb
“prakshh” <prakashdas18@gmail.com>
2022-01-18 16:28:23 +0530

    Filter product feature started

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,11 +1,25 @@
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
 
 const Container = styled.div``
-const Title = styled.h1``
-const FilterContainer = styled.div``
-const Filter = styled.div``
+const Title = styled.h1`
+    margin: 20px;
+`
+const FilterContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const Filter = styled.div`
+    margin: 20px;
+`
+const FilterText = styled.span`
+    font-size: 20px;
+    font-weight: 600;
+`
 
 const ProductList = () => {
     return (
@@ -14,9 +28,12 @@ const ProductList = () => {
             <Announcement />
             <Title>Dresses</Title>
             <FilterContainer>
-                <Filter>filter1</Filter>
-                <Filter>filter2</Filter>
+                <Filter><FilterText>Filter Products:</FilterText></Filter>
+                <Filter><FilterText>Sort Products:</FilterText></Filter>
             </FilterContainer>
+            <Products />
+            <Newsletter />
+            <Footer />
         </Container>
     )
 }
