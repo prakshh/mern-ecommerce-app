eae73c4ae3ce1a136bf9e419e43a9b153ecb9e86
“prakshh” <prakashdas18@gmail.com>
2022-01-18 20:40:35 +0530

    React Login and Register page design started

---
+++ src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const Container = styled.div``
+
+const Register = () => {
+    return (
+        <Container>Hello, I am from Register</Container>
+    )
+}
+
+export default Register
