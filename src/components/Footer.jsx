b0698c566952452025b7404b9fb3ece887c1ed21
“prakshh” <prakashdas18@gmail.com>
2022-01-18 12:52:58 +0530

    Footer center done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,16 +30,27 @@ const SocialIcon = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    margin: 20px;
+    margin-right: 20px;
 `;
 
 const Center = styled.div`
     flex: 1;
     padding: 20px;
 `;
-const Title = styled.h1``
-const List = styled.ul``
-const ListItem = styled.li``
+const Title = styled.h3`
+    margin-bottom: 30px;
+`
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`
 
 const Right = styled.div`
     flex: 1;
