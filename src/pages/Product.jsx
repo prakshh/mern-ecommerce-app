b2b6cdb0a819be0c5a684d5a75404a19e26705fc
“prakshh” <prakashdas18@gmail.com>
2022-01-18 16:42:05 +0530

    Product page started

---
+++ src/pages/Product.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const Container = styled.div``
+
+const Product = () => {
+    return (
+        <Container>hello from Product.jsx</Container>
+    )
+}
+
+export default Product
