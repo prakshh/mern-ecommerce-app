515b35d374c51f31250d041f67f85a6aa2eabd8a
“prakshh” <prakashdas18@gmail.com>
2022-01-18 12:35:36 +0530

    props color social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,24 +3,35 @@ import styled from 'styled-components';
 
 const Container = styled.div`
     display: flex;
-`
+`;
 const Left = styled.div`
     flex: 1;
     display: flex;
     flex-direction: column;
     padding: 20px;
-`
+`;
 const Logo = styled.h1``;
 
 const Desc = styled.p`
-  /* margin: 20px 0px; */
+  margin: 20px 0px;
 `;
 
 const SocialContainer = styled.div`
   display: flex;
+
 `;
 
-const SocialIcon = styled.div``
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${props=>props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 20px;
+`;
 
 const Center = styled.div`
     flex: 1;
@@ -38,16 +49,16 @@ const Footer = () => {
                 There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable.
                 </Desc>
                 <SocialContainer>
-                    <SocialIcon>
-                        <Facebook />
+                    <SocialIcon color="3B5999" >
+                        <Facebook/>
                     </SocialIcon>
-                    <SocialIcon>
+                    <SocialIcon color="E4405F" >
                         <Instagram />
                     </SocialIcon>
-                    <SocialIcon>
+                    <SocialIcon color="55ACEE" >
                         <Twitter />
                     </SocialIcon>
-                    <SocialIcon>
+                    <SocialIcon color="E60023" >
                         <Pinterest />
                     </SocialIcon>
                 </SocialContainer>
