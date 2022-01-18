4920ac53c37f842bbe53004e089e20c1cab70783
“prakshh” <prakashdas18@gmail.com>
2022-01-18 17:10:38 +0530

    adjusting image and desc

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,13 +5,32 @@ import Navbar from "../components/Navbar"
 import Newsletter from "../components/Newsletter"
 
 const Container = styled.div``
-const Wrapper = styled.div``
-const ImgContainer = styled.div``
-const Image = styled.img``
-const InfoContainer = styled.div``
-const Title = styled.h1``
-const Desc = styled.p``
-const Price = styled.span``
+const Wrapper = styled.div`
+    padding: 50px;
+    display: flex;
+`
+const ImgContainer = styled.div`
+    flex: 1;
+`
+const Image = styled.img`
+    width: 100%;
+    height: 90vh;
+    object-fit: cover;
+`
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 0px 50px;
+`
+const Title = styled.h1`
+    font-weight: 200;
+`
+const Desc = styled.p`
+    margin: 20px 0px;
+`
+const Price = styled.span`
+    font-weight: 100;
+    font-size: 40px;
+`
 
 const Product = () => {
     return (
