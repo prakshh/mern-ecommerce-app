057f11c65b57a18ce9473fd6e7f40d5d1603f0eb
“prakshh” <prakashdas18@gmail.com>
2022-01-20 21:15:54 +0530

    styling order summary

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -26,6 +26,8 @@ const TopButton = styled.button`
     background-color: ${props=>props.type === "filled" ? "black" : "transparent"};
     color: ${props=>props.type === "filled" && "white"};
 `
+const TopTexts = styled.span``
+
 const TopText = styled.span`
     text-decoration: underline;
     cursor: pointer;
@@ -93,12 +95,30 @@ const Hr = styled.hr`
 
 const Summary = styled.div`
     flex: 1;
+    border: 0.5px solid lightgray;
+    border-radius: 10px;
+    padding: 20px;
+    height: 50vh;
 `
-const SummaryTitle = styled.h1``
-const SummaryItem = styled.div``
+const SummaryTitle = styled.h1`
+    font-weight: 200;
+`
+const SummaryItem = styled.div`
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+    font-weight: ${props=>props.type === "total" && "500"};
+    font-size: ${props=>props.type === "total" && "24px"};
+`
 const SummaryItemText = styled.span``
 const SummaryItemPrice = styled.span``
-const Button = styled.button``
+const Button = styled.button`
+    width: 100%;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    font-weight: 600;
+`
 
 
 const Cart = () => {
@@ -110,8 +130,10 @@ const Cart = () => {
             <Title>YOUR BAG</Title>
             <Top>
                 <TopButton>CONTINUE SHOPPING</TopButton>
-                <TopText>Shopping Bag (2)</TopText>
-                <TopText>Your Wishlist (0)</TopText>
+                <TopTexts>
+                    <TopText>Shopping Bag (2)</TopText>
+                    <TopText>Your Wishlist (0)</TopText>
+                </TopTexts>
                 <TopButton type="filled">CHECKOUT NOW</TopButton>
             </Top>
             <Bottom>
@@ -172,8 +194,8 @@ const Cart = () => {
                         <SummaryItemText>Shipping Discount</SummaryItemText>
                         <SummaryItemPrice>$ -5.90</SummaryItemPrice>
                     </SummaryItem>
-                    <SummaryItem>
-                        <SummaryItemText type="total">Total</SummaryItemText>
+                    <SummaryItem type="total">
+                        <SummaryItemText>Total</SummaryItemText>
                         <SummaryItemPrice>$ 100</SummaryItemPrice>
                     </SummaryItem>
                     <Button>CHECKOUT NOW</Button>
