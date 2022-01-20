2b768249782b51d99d2f25ba6aa3c09c31225f94
“prakshh” <prakashdas18@gmail.com>
2022-01-20 17:44:28 +0530

    Cart topButton and topText

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,13 +4,45 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 const Container = styled.div``
+const Wrapper = styled.div`
+    padding: 20px;
+`
+const Title = styled.h1`
+    font-weight: 300;
+    text-align: center;
+`
+const Top = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+const TopButton = styled.button`
+    padding: 10px;
+    font-weight: 600;
+    cursor: pointer;
+`
+const TopText = styled.span`
+    text-decoration: underline;
+    cursor: pointer;
+    margin: 0px 10px;
+`
+const Bottom = styled.div``
 
 const Cart = () => {
   return (
       <Container>
           <Navbar />
           <Announcement />
-          cart
+          <Wrapper>
+            <Title>YOUR BAG</Title>
+            <Top>
+                <TopButton>CONTINUE SHOPPING</TopButton>
+                <TopText>Shopping Bag (2)</TopText>
+                <TopText>Your Wishlist (0)</TopText>
+                <TopButton>CHECKOUT NOW</TopButton>
+            </Top>
+            <Bottom></Bottom>
+        </Wrapper>
           <Footer />
       </Container>
   )
