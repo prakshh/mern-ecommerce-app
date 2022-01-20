4c6ce314b78eb18cd09784f3bac612f32539f272
“prakshh” <prakashdas18@gmail.com>
2022-01-20 16:34:14 +0530

    login page design started

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,65 @@
 import styled from "styled-components"
 
-const Container = styled.div``
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(255, 255, 255, 0.5),
+        rgba(255, 255, 255, 0.5)
+        ),
+        url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940") 
+        center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-size: cover;
+`
+const Wrapper = styled.div`
+    width: 40%;
+    padding: 20px;
+    background-color: white;
+`
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`
+const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+`
+
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0px 0px;
+    padding: 10px;
+`
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+`
+
+const Link = styled.a``   
 
 const Login = () => {
     return (
-        <Container></Container>
+        <Container>
+            <Wrapper>
+                <Title>SIGN IN</Title>
+                <Form>
+                    <Input placeholder="username" />
+                    <Input placeholder="password" />
+                    <Button>LOGIN</Button>
+                    <Link>DON"T REMEMBER PASSWORD?</Link>
+                    <Link>CREATE NEW ACCOUNT</Link>
+                </Form>
+            </Wrapper>
+      
+        </Container>
     )
 }
 
