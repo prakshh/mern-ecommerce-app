63a3e665eea3794615c9afa9a4fe12eb6fb95de7
“prakshh” <prakashdas18@gmail.com>
2022-01-20 23:17:30 +0530

    Navbar - mobile responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { mobile } from '../responsive'
 
 const Container = styled.div`
     height: 60px;
-    ${mobile({backgroundColor: "red"})}
+    ${mobile({height: "50px"})}
     /* @media only screen and (max-width: 300px) {
             display: none;
         } */
@@ -16,6 +16,7 @@ const Wrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    ${mobile({padding: "10px 0px"})}
 `;
 const Left = styled.div`
     flex: 1;
@@ -25,6 +26,7 @@ const Left = styled.div`
 const Language = styled.span`
     font-size: 14;
     cursor: pointer;
+    ${mobile({display: "none"})}
 `;
 const SearchContainer = styled.div`
     border: 0.5px solid lightgray;
@@ -35,6 +37,7 @@ const SearchContainer = styled.div`
 `;
 const Input = styled.input`
     border: none;
+    ${mobile({width: "50px"})}
 `;
 const Center = styled.div`
     flex: 1;
@@ -42,17 +45,20 @@ const Center = styled.div`
 `;
 const Logo = styled.h1`
     font-weight: bold;
+    ${mobile({fontSize: "24px"})}
 `;
 const Right = styled.div`
     flex: 1;
     display: flex;
     align-items: center;
     justify-content: flex-end;
+    ${mobile({flex: 2, justifyContent: "center"})}
 `;
 const MenuItem = styled.div`
     font-size: 14;
     cursor: pointer;
     margin-left: 25px;
+    ${mobile({fontSize: "12px", marginLeft: "10px"})}
 `;
 
 const Navbar = () => {
@@ -62,7 +68,7 @@ const Navbar = () => {
                 <Left>
                     <Language>EN</Language>
                     <SearchContainer>
-                        <Input /> 
+                        <Input placeholder="Search" /> 
                         <Search style={{color:"gray", fontSize:16}} />
                     </SearchContainer>
                 </Left>
