736ef7f9f6b49fdaf37012dee85d15a26ff374f1
“prakshh” <prakashdas18@gmail.com>
2022-01-21 20:08:28 +0530

    Node.js MongoDB Connection

--- eCom-api/index.js
+++ eCom-api/index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://lama:lama987@cluster0.owlmy.mongodb.net/shop?retryWrites=true&w=majority"
+)
+.then(()=>console.log("DB Connection Successfull")).catch((err)=> {
+    console.log(err);
+});
 
 app.listen(5000, () => {
     console.log("Backend server is running!");
