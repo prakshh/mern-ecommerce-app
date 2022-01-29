81a5ef9dae8e540d94f2f3ad066f61ca561ea4ce
Prakash Das <prakashdas18@gmail.com>
2022-01-29 09:46:46 +0530

    React Redux Toolkit Authentication

--- eCom-fullstack/client/src/redux/store.js
+++ eCom-fullstack/client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
+import userRedux from "./userRedux";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    user: userRedux
   }
 });
