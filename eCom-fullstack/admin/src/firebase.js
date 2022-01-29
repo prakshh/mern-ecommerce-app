2fff84ae3a1377924414a1235f1d4031383ee71f
Prakash Das <prakashdas18@gmail.com>
2022-01-29 20:47:22 +0530

    React Firebase

---
+++ eCom-fullstack/admin/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_ojIkMakei3ZeduMWzKnwmpQ1rJ4Iw34",
+  authDomain: "shop-82615.firebaseapp.com",
+  projectId: "shop-82615",
+  storageBucket: "shop-82615.appspot.com",
+  messagingSenderId: "704576753495",
+  appId: "1:704576753495:web:440e3be646718fe22eb812"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
+
